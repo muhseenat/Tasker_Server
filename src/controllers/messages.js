1c1fbda9f56b5761a1dd3195a29e41781d89105a
muhseenat <mucktd777@gmail.com>
2022-04-11 09:50:23 +0530

    conversation creation ttme problem solved

--- src/controllers/messages.js
+++ src/controllers/messages.js
@@ -11,9 +11,7 @@ const messageFunction=(req,res)=>{
 //GET MESSAGE CONTROLLER
 
 const getMessageFunction=(req,res)=>{
-    console.log('get medddasooooo');
     const {id} =req.params
-    console.log(id,'id ooooooo');
     getMessage(id).then((data)=>{
         res.send(data)
     }).catch(err=>res.code(400).send(err))
--- src/helpers/conversation.js
+++ src/helpers/conversation.js
@@ -1,31 +1,42 @@
 const Conversation = require('../model/conversationSchema')
 
-module.exports={
+module.exports = {
 
     //CREATE CONVERSATION
- createConversation:(data)=>{
-     console.log(data,'this is data');
-     return new Promise((resolve,reject)=>{
-         const newConversation = new Conversation(
-             {
-                 members:[data.senderId,data.receiverId]
-             }
-         );
-         newConversation.save().then((resp)=>{
-            resolve(resp)
-         }).catch(err=>reject(err))
-     })
- },
-//GET CONVERSATION FROM DB
- getConversation:(id)=>{
-     return new Promise((resolve,reject)=>{
-         Conversation.find({
-             members:{$in:[id]}
-         }).then((data)=>{
-             resolve(data)
-         }).catch(err=>reject(err))
-     })
- }
+    createConversation: (data) => {
+        console.log(data, 'this is data');
+        return new Promise(async (resolve, reject) => {
+            const exist = await Conversation.findOne({
+                members: { $all: [data.receiverId, data.senderId] }
+            })
+            if (exist) {
+                resolve(exist)
+                console.log('ind mone');
+            } else {
+                const newConversation = new Conversation(
+                    {
+                        members: [data.senderId, data.receiverId]
+                    }
+                );
+                newConversation.save().then((resp) => {
+                    resolve(resp)
+                }).catch(err => reject(err))
+            }
+
+        })
+
+
+    },
+    //GET CONVERSATION FROM DB
+    getConversation: (id) => {
+        return new Promise((resolve, reject) => {
+            Conversation.find({
+                members: { $in: [id] }
+            }).then((data) => {
+                resolve(data)
+            }).catch(err => reject(err))
+        })
+    }
 
 
 
