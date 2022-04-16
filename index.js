60fa1a3cb23984abd9037101e991a9ab3c879067
muhseenat <muhseenat@protonmail.com>
2022-04-16 12:02:32 +0530

    updated

--- index.js
+++ index.js
@@ -14,8 +14,51 @@ const fastify = require("fastify")({
             : false
       }
   })
+// socket server
+const io = require('socket.io')(fastify.server);
+ 
+
+let users = []
+
+//Function to avoid duplicate users while pushing to array and add user
+const addUser = (userId, socketId) => {
+    !users.some((user) => user.userId === userId) &&
+        users.push({ userId, socketId })
+};
+//Function to remove user when disconnect
+const removeUser = (socketId) => {
+    users = users.filter(user => user.socketId !== socketId)
+}
+
+//Function to get users
+const getUser=(userId)=>{
+    return users.find(user=>user.userId === userId);
+}
+
+io.on("connection", (socket) => {
+    console.log('a user connected');
+    //TAKE USERID AND SOCKETID FROM USER
+    socket.on('addUser', (userId) => {
+        addUser(userId, socket.id);
+        io.emit('getUsers', users);
+    });
+
+    //Send and get messages
+    socket.on('sendMessage',({senderId,receiverId,text})=>{
+        const user=getUser(receiverId);
+        io.to(user?.socketId).emit('getMessage',{
+            senderId,
+            text,
+        });
+    });
 
 
+    //When disconnect user
+    socket.on("disconnect", () => {
+        io.emit('getUsers', users)
+        removeUser(socket.id);
+    })
+})
 //SWAGGER FOR API DOCUMENTATION
 fastify.register(require("fastify-swagger"),{
     
